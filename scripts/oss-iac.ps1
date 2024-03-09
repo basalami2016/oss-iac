14321617916a9c6a8b7d52c8a810786155d8ba02
BILIAMINU SALAMI <basalami2016@gmail.com>
2024-03-09 10:52:25 -0500

    makes changes

--- scripts/oss-iac.ps1
+++ scripts/oss-iac.ps1
@@ -2,13 +2,15 @@ $m = New-Module -ScriptBlock {
 
   $url =  "https://dev.azure.com/niqflex-org/niqflex/_apis/wit/queries?api-version=7.1-preview.2"
 
+  az boards query --wiql "SELECT [System.Title], [System.Description] FROM workitems" --output table --org https://dev.azure.com/niqflex-org
+  
   #Invoke-RestMethod -Uri $url | 
     #Format-Table -Property  [System.Title],
 
   $output = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json"  #-Headers $header 
   Write-Output $output.value
   $output.value | ForEach-Object {
-        Write-Host $_.System.Title
+      Write-Host $_.System.Title
   }
 
   function Hello ($name) {
